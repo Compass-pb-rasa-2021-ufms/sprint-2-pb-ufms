64486404c48933537e9746b332ce177f3b392290
Leonardo Oliveira <54418482+Leo0liveira@users.noreply.github.com>
2021-11-29 18:24:43 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB(){
-    await mongoose.connect('mongodb://root:root@weathertracker-db:27017');
+    await mongoose.connect('mongodb://root:root@dbmongo:27017/');
 }
 
 module.exports = connectDB;
